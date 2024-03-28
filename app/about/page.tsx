9759710706173bb05e7178062a843cb60913e47f
lukasbostick <lukasbostick@gmail.com>
2024-03-28 08:25:26 -0400

    Added images to About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+import Image from 'next/image';
+
 export default function Page() {
     return (
         <div className="flex flex-col items-center p-4">
@@ -5,6 +8,22 @@ export default function Page() {
             <p className="text-lg max-w-prose">
                 At Team Golf, we are more than just a coffee company – we are passionate advocates for a sustainable and ethical coffee industry. We understand the profound impact the coffee industry can have on the environment, and we are dedicated to minimizing our footprint. From environmentally friendly farming practices to responsible packaging, sustainability is integrated into every aspect of our operations.
             </p>
+            <div className="flex flex-row justify-center align-items: center gap-6 p-6">
+                <Image
+                    src='/food2.jpg'
+                    width={500}
+                    height={500}
+                    className='hidden md:block rounded-lg width-2/5'
+                    alt='Food2'
+                />
+                <Image
+                    src='/food3.jpg'
+                    width={500}
+                    height={500}
+                    className='hidden md:block rounded-lg width-2/5'
+                    alt='Food3'
+                />
+            </div>
         </div>
     );
 }
